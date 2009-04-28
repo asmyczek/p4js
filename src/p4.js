944286a032570c2d2bc9213bca925b4c226e5d83
asmyczek <adam.smyczek@gmail.com>
2009-04-28 08:47:12 -0700

    Finished basic combinators.

--- src/p4.js
+++ src/p4.js
@@ -21,6 +21,13 @@ var $P = function() {
 		return p;
 	};
 
+  var runParser = function(p) {
+      pushValue(p.parse(_state.input, _state.data, _state.line, _state.column).value()); 
+      _state.input = p.input();
+      _state.line = p.line();
+      _state.column = p.column();
+  };
+
 	var error = function(err) {
 		return { type    : "parse_error"
            , message : err
@@ -63,14 +70,15 @@ var $P = function() {
         if (!_state.input || _state.input === '') {
           throw error("Empty input!");
         } else {
-          var v = _state.input[0];
-          if (isEqual(v, "\n")) {
+          var ch = _state.input[0];
+          pushValue(ch); 
+          _state.input = _state.input.slice(1); 
+          if (isEqual(ch, "\n")) {
             _state.line++;
             _state.column = 0;
           } else {
             _state.column++;
           }
-          pushValue(v); _state.input = _state.input.slice(1); 
         }
       });
 		},
@@ -89,16 +97,15 @@ var $P = function() {
 		_choice : function() {
 			var ps = Array.prototype.slice.apply(arguments);
 			return pushParser(function() { 
-        for (var i = 0; i < ps.lenght; i++) {
+        for (var i = 0; i < ps.length; i++) {
 					try {
-						pushValue(ps[i].parse(_state.input).value()); 
-						_state.input = ps[i].input();
+            runParser(ps[i]);
 						return;
 					} catch (e) {
 					  if (!e.type || e.type !== "parse_error") throw e;
 					}
         }
-				throw error("No parser match!");
+				throw error("No parser match in 'choice'!");
 			});
 		},
 
@@ -114,12 +121,7 @@ var $P = function() {
     },
 
 		bind : function(p) {
-			return pushParser(function() { 
-        pushValue(p.parse(_state.input, _state.data, _state.line, _state.column).value()); 
-        _state.input = p.input();
-        _state.line = p.line();
-        _state.column = p.column();
-      });
+			return pushParser(function() { runParser(p); });
 		},
 
 		_do : function() {
@@ -142,14 +144,32 @@ var $P = function() {
 		},
 
     _many : function(p) {
-      return this._choice(this._many1(p), this._return([]));
+      return pushParser(function() { try { while (true) runParser(p); } catch (e) { } });
     },
 
     _many1 : function(p) {
-      var that = this;
-      return pushParser(function() {
-        return that.bind(p).parse(_state.input).value();
-      });
+      return this.bind(p)._many(p);
+    },
+
+    _manyTill : function(p, b) {
+      var rec = function(p, b) {
+        try {
+          b.parse(_state.input);
+          return;
+        } catch (e) {
+          runParser(p);
+          rec(p, b);
+        }
+      };
+      return pushParser(function() { rec(p, b); });
+    },
+
+    _oneOf : function(match) {
+      return this._sat(function(c) { return match.indexOf(c) != -1; }, "Not OneOf '" + match + "'!");
+    },
+
+    _noneOf : function(match) {
+      return this._sat(function(c) { return match.indexOf(c) == -1; }, "Not NoneOf '" + match + "'!");
     },
 
 		parse : function(input, data, line, column) {
