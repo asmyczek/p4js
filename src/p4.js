0bcc07a267db0fcd33bcad17dc6b19a63b4024e6
asmyczek <adam.smyczek@gmail.com>
2009-04-30 09:17:37 -0700

    Extracting parser lib from general context.

--- src/p4.js
+++ src/p4.js
@@ -1,208 +1,209 @@
-var $P = function() {
+var P4JS = $P = function() {
 
-	var _parser_stack = [],
-      _value_stack,
-      _state; 
-
-	var pushValue = function(v) {
-		_value_stack[_value_stack.length - 1].push(v);
-	};
-
-	var addValueStack = function() {
-		_value_stack.push([]);
-	};
-
-	var reduceValueStack = function(f) {
-		pushValue(f(_value_stack.pop()));
-	};
-
-	var pushParser = function(f) {
-		_parser_stack.push(f);
-		return p;
-	};
-
-  var runParser = function(p) {
-      pushValue(p.parse(_state.input, _state.data, _state.line, _state.column).value()); 
-      _state.input = p.input();
-      _state.line = p.line();
-      _state.column = p.column();
+  var createState = function(input, data) {
+    return { input    : input
+           , line     : 1 
+           , column   : 0
+           , data     : data
+           , nextLine : function()  { this.line++; this.column = 0; }
+           , nextChar : function () { this.column++; }};
   };
 
-	var error = function(err) {
-		return { type    : "parse_error"
-           , message : err
-           , line    : _state.line
-           , column  : _state.column
-           , input   : _state.input
-           , print   : function() {
-             return "Parser Error: " + err + " at (line " + _state.line + ", column " + _state.column + ")";
-           }
-          };
-	};
+  var createContext = function(lib) {
 
-	var curry = function(f) {
-		return function() {
-			var args = Array.prototype.slice.apply(arguments);
-			return function () {
-				return f.apply(null, args.concat(Array.prototype.slice.apply(arguments)));
-			};
-		};
-	};
+    var parser_stack = [];
+    var value_stack  = [];
 
-	// Sat functions
-	var isDigit    = function(c)    { return ((c >= "0") && (c <= "9")); };
-	var isLower    = function(c)    { return ((c >= "a") && (c <= "z")); };
-	var isUpper    = function(c)    { return ((c >= "A") && (c <= "Z")); };
-	var isAlpha    = function(c)    { return isLower(c) || isUpper(c); };
-	var isAlphaNum = function(c)    { return isAlpha(c) || isDigit(c); };
-	var isSpace    = function(c)    { return ((c === ' ') || (c === '\t')); };
-	var isEqual    = function(a, b) { return a === b };
+    var pushValue = function(v) {
+      value_stack[value_stack.length - 1].push(v);
+    };
 
-	// Parser
-	var p = {
+    var C = function () {};
+    C.prototype = lib;
+    var c = new C();
 
-		_return : function(v) {
-			return pushParser(function() { pushValue(v); });
-		},
+    c.state = undefined;
 
-		_item : function() {
-			return pushParser(function() { 
-        if (!_state.input || _state.input === '') {
-          throw error("Empty input!");
-        } else {
-          var ch = _state.input[0];
-          pushValue(ch); 
-          _state.input = _state.input.slice(1); 
-          if (isEqual(ch, "\n")) {
-            _state.line++;
-            _state.column = 0;
-          } else {
-            _state.column++;
-          }
-        }
-      });
-		},
+    c.pushValue = pushValue;
 
-		_sat : function(f, error_msg) {
-			return this._do()._item().reduce(function(vs) { if (f(vs[0])) { return vs[0]; } else { throw error(error_msg);} });
-		},
+    c.addValueStack = function() {
+      return this.pushParser(function() { value_stack.push([]); });
+    };
 
-		_digit 		: function () { return this._sat(isDigit, 	 'not a digit!'); },
-		_lower 		: function () { return this._sat(isLower, 	 'not a lower char!'); },
-		_upper 		: function () { return this._sat(isUpper, 	 'not an upper char!'); },
-		_alpha 		: function () { return this._sat(isAlpha, 	 'not an alpha char!'); },
-		_alphanum	: function () { return this._sat(isAlphaNum, 'not an alpha-num char!'); },
-		_space 		: function () { return this._sat(isSpace, 	 'not a space char!'); },
+    c.reduceValueStack = function(f) {
+      return this.pushParser(function() { pushValue(f(value_stack.pop())) });
+    };
 
-		_choice : function() {
-			var ps = Array.prototype.slice.apply(arguments);
-			return pushParser(function() { 
-        for (var i = 0; i < ps.length; i++) {
-					try {
-            runParser(ps[i]);
-						return;
-					} catch (e) {
-					  if (!e.type || e.type !== "parse_error") throw e;
-					}
-        }
-				throw error("No parser match in 'choice'!");
-			});
-		},
-
-		_char : function(c) { 
-			return this._sat(curry(isEqual)(c), "Expecting a '" + c + "'!"); 
-		},
-
-    _string : function(s) {
-      var p = this._do();
-      for (var i = 0; i < s.length; i++) p = p._char(s[i]);
-      p.join();
+    c.pushParser = function(f) {
+      parser_stack.push(f);
       return this;
-    },
+    };
 
-		bind : function(p) {
-			return pushParser(function() { runParser(p); });
-		},
+    c.runParser = function(p) {
+        this.pushValue(p.parseWithState(this.state).value()); 
+        this.state = p.state;
+    };
 
-		_do : function() {
-			return pushParser(function() { addValueStack(); });
-		},
+    c.error = function(err) {
+      return { type    : "parse_error"
+             , message : err
+             , state   : this.state
+             , print   : function() {
+               return "Parser Error: " + err + " at (line " + this.state.line + ", column " + this.state.column + ")";
+             } };
+    };
 
-		reduce : function(f) {
-			return pushParser(function() { reduceValueStack(f); });
-		},
+    c.curry = function(f) {
+      return function() {
+        var args = Array.prototype.slice.apply(arguments);
+        return function () {
+          return f.apply(null, args.concat(Array.prototype.slice.apply(arguments)));
+        };
+      };
+    };
 
-		join : function(c) {
-			return pushParser(function() { reduceValueStack(function(vs) { return vs.join(c || ''); }); });
-		},
+    c.isDigit    = function(c)    { return ((c >= "0") && (c <= "9")); };
+    c.isLower    = function(c)    { return ((c >= "a") && (c <= "z")); };
+    c.isUpper    = function(c)    { return ((c >= "A") && (c <= "Z")); };
+    c.isAlpha    = function(c)    { return isLower(c) || isUpper(c); };
+    c.isAlphaNum = function(c)    { return isAlpha(c) || isDigit(c); };
+    c.isSpace    = function(c)    { return ((c === ' ') || (c === '\t')); };
+    c.isEqual    = function(a, b) { return a === b };
 
-		int : function(f) {
-			return pushParser(function() { reduceValueStack(function(vs) { 
-          var v = parseInt(vs.join(''));
-          return (!f)? v : f(v);
-				}); });
-		},
+    c.parse = function(input, data) {
+      return this.parseWithState(createState(input, data));
+    };
 
-    _many : function(p) {
-      return pushParser(function() { try { while (true) runParser(p); } catch (e) { } });
-    },
+    c.parseWithState = function(state) {
+      this.state = state;
+      value_stack  = [];
+      value_stack.push([]);
+      for (var i = 0; i < parser_stack.length; i++) {
+        parser_stack[i].apply(this);
+      }
+      return this;
+    };
 
-    _many1 : function(p) {
-      return this.bind(p)._many(p);
-    },
+    c.value = function() {
+      return value_stack[0];
+    };
 
-    _manyTill : function(p, b) {
-      var rec = function(p, b) {
+    c.input = function() {
+      return this.state.input;
+    };
+
+    return c;
+  };
+
+  var P = function() {};
+  P.prototype = createContext(P4JS.lib);
+  return new P();
+};
+
+
+P4JS.lib = {
+
+  _return : function(v) {
+    return this.pushParser(function() { this.pushValue(v); });
+  },
+
+  _item : function() {
+    return this.pushParser(function() { 
+      var s = this.state;
+      if (!s.input || s.input === '') {
+        throw this.error("Empty input!");
+      } else {
+        var ch = s.input[0];
+        this.pushValue(ch); 
+        s.input = s.input.slice(1); 
+        (this.isEqual(ch, "\n"))? s.nextLine() : s.nextChar();
+      }
+    });
+  },
+
+  _sat : function(f, error_msg) {
+    return this._do()._item().reduce(function(vs) { if (f(vs[0])) { return vs[0]; } else { throw this.error(error_msg);} });
+  },
+
+  _digit 		: function () { return this._sat(this.isDigit, 	 'not a digit!'); },
+  _lower 		: function () { return this._sat(this.isLower, 	 'not a lower char!'); },
+  _upper 		: function () { return this._sat(this.isUpper, 	 'not an upper char!'); },
+  _alpha 		: function () { return this._sat(this.isAlpha, 	 'not an alpha char!'); },
+  _alphanum	: function () { return this._sat(this.isAlphaNum, 'not an alpha-num char!'); },
+  _space 		: function () { return this._sat(this.isSpace, 	 'not a space char!'); },
+
+  _choice : function() {
+    var ps = Array.prototype.slice.apply(arguments);
+    return this.pushParser(function() { 
+      for (var i = 0; i < ps.length; i++) {
         try {
-          b.parse(_state.input);
+          this.runParser(ps[i]);
           return;
         } catch (e) {
-          runParser(p);
-          rec(p, b);
+          if (!e.type || e.type !== "parse_error") throw e;
         }
-      };
-      return pushParser(function() { rec(p, b); });
-    },
+      }
+      throw this.error("No parser match in 'choice'!");
+    });
+  },
 
-    _oneOf : function(match) {
-      return this._sat(function(c) { return match.indexOf(c) != -1; }, "Not OneOf '" + match + "'!");
-    },
+  _char : function(c) { 
+    return this._sat(this.curry(this.isEqual)(c), "Expecting a '" + c + "'!"); 
+  },
 
-    _noneOf : function(match) {
-      return this._sat(function(c) { return match.indexOf(c) == -1; }, "Not NoneOf '" + match + "'!");
-    },
+  _string : function(s) {
+    var p = this._do();
+    for (var i = 0; i < s.length; i++) p = p._char(s[i]);
+    p.join();
+    return this;
+  },
 
-		parse : function(input, data, line, column) {
-      _state = { input  : input
-               , line   : line || 1 
-               , column : column || 0
-               , data   : data };
-			_value_stack  = [];
-			addValueStack();
-			for (var i = 0; i < _parser_stack.length; i++) {
-				_parser_stack[i].call();
-			}
-			return this;
-		},
+  bind : function(p) {
+    return this.pushParser(function() { this.runParser(p); });
+  },
 
-		value : function() {
-			return _value_stack[0];
-		},
+  _do : function() { return this.addValueStack(); },
 
-		input : function() {
-			return _state.input;
-		},
+  reduce : function(f) { return this.reduceValueStack(f); },
 
-    line : function() {
-      return _state.line;
-    },
+  join : function(c) { return this.reduceValueStack(function(vs) { return vs.join(c || ''); }); },
 
-    column : function() {
-      return _state.column;
-    },
+  int : function(f) {
+    return this.reduceValueStack(function(vs) { 
+      var v = parseInt(vs.join(''));
+      return (!f)? v : f(v);
+      });
+  },
 
-	};
+  _many : function(p) {
+    return this.pushParser(function() { try { while (true) this.runParser(p); } catch (e) { } });
+  },
 
-	return p;
+  _many1 : function(p) {
+    return this.bind(p)._many(p);
+  },
+
+  _manyTill : function(p, b) {
+    var that = this;
+    var rec = function(p, b) {
+      try {
+        b.parse(that.state.input);
+        return;
+      } catch (e) {
+        that.runParser(p);
+        rec(p, b);
+      }
+    };
+    return this.pushParser(function() { rec(p, b); });
+  },
+
+  _oneOf : function(match) {
+    return this._sat(function(c) { return match.indexOf(c) != -1; }, "Not OneOf '" + match + "'!");
+  },
+
+  _noneOf : function(match) {
+    return this._sat(function(c) { return match.indexOf(c) == -1; }, "Not NoneOf '" + match + "'!");
+  },
+
 };
 
